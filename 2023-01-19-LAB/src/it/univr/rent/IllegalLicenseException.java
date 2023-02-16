aae6633a7638a242632663eec4eb3f2fd52d75e6
Vittorio Maria Stano <69857386+stano-vittorio@users.noreply.github.com>
2023-02-16 16:08:19 +0100

    Delete 2023-01-19-LAB directory

--- 2023-01-19-LAB/src/it/univr/rent/IllegalLicenseException.java
+++
@@ -1,9 +0,0 @@
-package it.univr.rent;
-
-public class IllegalLicenseException extends IllegalArgumentException {
-	
-	private static final long serialVersionUID = 1L;
-
-	public IllegalLicenseException() {	
-	}
-}
